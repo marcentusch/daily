a158f86568d5577301fc2fe291da39434d2ea08a
marcentusch <marcentusch@hotmail.com>
2019-01-19 21:40:43 +0100

    added login view

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700');
+
+$primary: #FDE5DB;
+$secondary: #242635;
+$accent: #BEFEE9;
+
+$roboto-400: 'Roboto Condensed', sans-serif;
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Condensed', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
